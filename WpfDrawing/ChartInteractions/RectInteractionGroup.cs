b79aed9319ff9c3316ac1043d3bd60db4b1229d5
eyouyou <393370459@qq.com>
2020-02-25 21:07:04 +0800

    1.增加RectInteractionGroup resize

--- WpfDrawing/ChartInteractions/RectInteractionGroup.cs
+++ WpfDrawing/ChartInteractions/RectInteractionGroup.cs
@@ -21,63 +21,12 @@ namespace HevoDrawing.Interactions
         ComponentId IdGenerater = new ComponentId();
         double ColPercentage = 0.0;
         double RowPercentage = 0.0;
-        public RectInteractionGroup(AxisInteractionCanvas interaction, int col = 1, int row = 1, params RectDrawingCanvasContainer[] canvas)
+        InteractionCanvas GlobalInteraction = null;
+        public RectInteractionGroup(InteractionCanvas interaction, int col = 1, int row = 1, params RectDrawingCanvasContainer[] canvas)
         {
             InteractionVisuals = new List<InteractionCanvas>();
-            Containers = new List<RectDrawingCanvasContainer>();
-            if (interaction != null)
-            {
-                InteractionVisuals.Add(interaction);
-                interaction.ParentElement = this;
-            }
-
-            ColPercentage = 1.0 / col;
-            for (int i = 0; i < col; i++)
-            {
-                ContainerGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(ColPercentage, GridUnitType.Star) });
-            }
-
-            RowPercentage = 1.0 / row;
-            for (int i = 0; i < row; i++)
-            {
-                ContainerGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(RowPercentage, GridUnitType.Star) });
-            }
-            var index = 0;
-            foreach (var item in canvas)
-            {
-                var c = item.DrawingCanvas;
-                ContainerGrid.Children.Add(item);
-                Containers.Add(item);
-                c.Col = index % col;
-                c.Row = index / col;
-                SetColumn(item, c.Col);
-                SetRow(item, c.Row);
-                if (c.Id == -1)
-                {
-                    c.Id = IdGenerater.GenerateId();
-                }
-                if (interaction != null)
-                {
-                    interaction.DataSources.Add(c.Id, c.DataSource);
-                    interaction.DependencyContainers.Add(c.Id, item);
-                    c.InteractionCanvas = interaction;
-                }
-                else
-                {
-                    // 送默认AxisInteractionCanvas
-                    var interaction2 = item.InteractionCanvas;
-                    if (interaction2 != null)
-                    {
-                        interaction2.DependencyContainers.Add(c.Id, item);
-                        interaction2.DataSources.Add(c.Id, c.DataSource);
-                        interaction2.ParentElement = item;
-                        c.InteractionCanvas = interaction2;
-                        item.EnableInteraction();
-                        InteractionVisuals.Add(interaction2);
-                    }
-                }
-                index++;
-            }
+            Containers = canvas.ToList();
+            GlobalInteraction = interaction;
             if (interaction != null)
             {
                 SetColumn(interaction, 0);
@@ -90,11 +39,79 @@ namespace HevoDrawing.Interactions
             {
                 SizeChanged += RectInteractionGroup_SizeChanged2;
             }
+            Resize(col, row);
+
             Canvas = canvas.ToDictionary(it => it.DrawingCanvas.Id, it => it.DrawingCanvas);
 
             Children.Add(ContainerGrid);
 
             EnableInteraction = true;
+        }
+        public void Resize(int col, int row)
+        {
+            ColPercentage = 1.0 / col;
+            ContainerGrid.ColumnDefinitions.Clear();
+            for (int i = 0; i < col; i++)
+            {
+                ContainerGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(ColPercentage, GridUnitType.Star) });
+            }
+
+            RowPercentage = 1.0 / row;
+            ContainerGrid.RowDefinitions.Clear();
+            for (int i = 0; i < row; i++)
+            {
+                ContainerGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(RowPercentage, GridUnitType.Star) });
+            }
+
+            InteractionVisuals.Clear();
+            if (GlobalInteraction != null)
+            {
+                InteractionVisuals.Add(GlobalInteraction);
+                GlobalInteraction.ParentElement = this;
+            }
+
+            var index = 0;
+
+            foreach (var item in Containers)
+            {
+                var c = item.DrawingCanvas;
+                ContainerGrid.Children.Add(item);
+                c.Col = index % col;
+                c.Row = index / col;
+                SetColumn(item, c.Col);
+                SetRow(item, c.Row);
+                if (c.Id == -1)
+                {
+                    c.Id = IdGenerater.GenerateId();
+                }
+                if (GlobalInteraction != null)
+                {
+                    GlobalInteraction.DataSources.Clear();
+                    GlobalInteraction.DataSources.Add(c.Id, c.DataSource);
+                    GlobalInteraction.DependencyContainers.Clear();
+                    GlobalInteraction.DependencyContainers.Add(c.Id, item);
+                    c.InteractionCanvas = GlobalInteraction;
+                }
+                else
+                {
+                    // 送默认AxisInteractionCanvas
+                    var interaction2 = item.InteractionCanvas;
+                    if (interaction2 != null)
+                    {
+                        interaction2.DependencyContainers.Clear();
+                        interaction2.DependencyContainers.Add(c.Id, item);
+                        interaction2.DataSources.Clear();
+                        interaction2.DataSources.Add(c.Id, c.DataSource);
+                        interaction2.ParentElement = item;
+                        c.InteractionCanvas = interaction2;
+                        item.DisableInteraction();
+                        item.EnableInteraction();
+                        InteractionVisuals.Add(interaction2);
+                    }
+                }
+                index++;
+            }
+
         }
         public RectInteractionGroup(int col = 1, int row = 1, params RectDrawingCanvasContainer[] canvas)
         {
